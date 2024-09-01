fb59ba1b8954670a7be1f41c70dbabdb25d929dd
Qiutong Zeng <zengqiutong@outlook.com>
2024-09-01 17:24:05 +1000

    Updated GameController.java and added some method for gameStart and player moving

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -4,15 +4,19 @@ import Model.Maze;
 import Model.Player;
 import View.Viewer;
 
+import java.util.Scanner;
+
 public class GameController {
     private Maze maze;
     private Player player;
     private Viewer viewer;
+    private Scanner scanner;
 
     public GameController(Maze maze, Player player, Viewer viewer){
        this.maze = maze;
        this.player = player;
        this.viewer = viewer;
+       this.scanner = new Scanner(System.in);
 
         int[] playerPosition = maze.getPlayerPosition();
         player.setPosition(playerPosition[0], playerPosition[1]);
@@ -20,10 +24,33 @@ public class GameController {
 
     public void StartGame(){
         //TODO
+         while (true){
+             viewer.printMap();
+             System.out.println("Enter w/a/s/d for move");
+             String move = scanner.nextLine();
+
+             //TODO it was fixed coor for current stage, it need to be changed later
+             if (move.equals("w")){
+                 playerMove(-1, 0);
+             } else if (move.equals("a")) {
+                 playerMove(0, -1);
+             } else if (move.equals("s")) {
+                 playerMove(1, 0);
+             } else if (move.equals("d")) {
+                 playerMove(0, 1);
+             } else {
+                 System.out.println("Invalid move. Use w/a/s/d to move.");
+             }
+
+         }
+         
+         
+         
     }
 
     private void playerMove(int dx, int dy){
         //TODO
+        //Update player position on "waze" and print when player encounter enemy or items
     }
 
 

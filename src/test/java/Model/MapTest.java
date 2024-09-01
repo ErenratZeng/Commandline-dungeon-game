ea85ecc4dcb8d686b2c2df909dd0b9518849f3e2
Qiutong Zeng <zengqiutong@outlook.com>
2024-09-01 11:17:50 +1000

    Add MapTest.java

---
+++ src/test/java/Model/MapTest.java
@@ -0,0 +1,16 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapTest {
+
+    @Test
+    void load_map() {
+    }
+
+    @Test
+    void printMap() {
+    }
+}

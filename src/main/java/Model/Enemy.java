f28faf3136ba460e5feb2372bb02f8923e462de0
Qiutong Zeng <zengqiutong@outlook.com>
2024-09-01 15:01:16 +1000

    Update Skeleton, added some method for it

--- src/main/java/Model/Enemy.java
+++ src/main/java/Model/Enemy.java
@@ -1,4 +1,24 @@
 package Model;
 
+import java.util.Random;
+
 public class Enemy extends Character {
+    public Enemy(int health) {
+        super(health);
+    }
+
+    @Override
+    public int getHealth() {
+        return super.getHealth();
+    }
+
+    public int setEnemyHealth(){
+        Random random = new Random();
+        int enemyHealth = random.nextInt(7);
+        if (enemyHealth == 0){
+            return enemyHealth + 1;
+        }
+        return enemyHealth;
+    }
+
 }
--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,4 +1,25 @@
 package Model;
 
-public class Player {
+import java.util.ArrayList;
+
+public class Player extends Character{
+    ArrayList inventory;
+    public Player(int health) {
+        super(health);
+        inventory = new ArrayList();
+    }
+
+    @Override
+    public int getHealth() {
+        return super.getHealth();
+    }
+
+    public int heal(int amount){
+        return getHealth() + amount;
+    }
+
+    public void use_item(String item){
+        //TODO
+    }
+
 }

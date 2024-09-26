ab035bb4907630c1f298e6cc3c1991e1ba66a7f8
Christo Joby Antony <christotantony2003@gmail.com>
2024-09-26 13:42:05 +1000

    Update player object on level change

--- src/main/java/Game/Model/State/GameLevel.java
+++ src/main/java/Game/Model/State/GameLevel.java
@@ -2,14 +2,21 @@ package Game.Model.State;
 
 import Engine.Model.GameState;
 
+import java.util.concurrent.Callable;
+
 public class GameLevel extends GameState<Integer> {
 
+    Runnable onLevelChange;
     public GameLevel() {
         super(1);
     }
 
     public void nextLevel() {
         this.value ++;
+        if (onLevelChange != null) onLevelChange.run();
+    }
+    public void onLevelChange (Runnable onLevelChange) {
+        this.onLevelChange = onLevelChange;
     }
 
 }

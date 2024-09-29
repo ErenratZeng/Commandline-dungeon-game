6aa3ef5ace6ca6a9c0b4cf47ed0f2b1d3da8fb78
u7140320 <u7140320@anu.edu.au>
2024-09-29 20:07:36 +1000

    Migrated JavaDocs to JavaDocs branch.

--- src/main/java/Engine/Model/Direction.java
+++ src/main/java/Engine/Model/Direction.java
@@ -1,5 +1,10 @@
 package Engine.Model;
 
+/**
+ * Enum class for the four possible directions the player can move in.
+ *
+ * Written by Qiutong Zeng.
+ */
 public enum Direction {
     UP, DOWN, LEFT, RIGHT;
 }

962c9994f33a195821a58d05ed1e9eee9f704ada
u7140320 <u7140320@anu.edu.au>
2024-09-29 15:26:26 +1000

    Completed JavaDocs and refactored the print text. Test commit 3.

--- src/main/java/Game/Model/State/GameWinState.java
+++ src/main/java/Game/Model/State/GameWinState.java
@@ -3,23 +3,43 @@ package Game.Model.State;
 import Engine.Model.GameState;
 import Game.Game;
 
+/**
+ * A simple class that determines whether a playthrough is ongoing, won or
+ * lost.
+ *
+ * Written by Christo Antony.
+ */
 public class GameWinState extends GameState<GameWinState.WinState> {
 
+    /**
+     * Implements the three independent and possible states of a playthrough.
+     */
     public enum WinState {
         PLAYER_WIN,
         PLAYER_LOSE,
         ONGOING,
     }
+
+    /**
+     * Constructor for the class which initialises a new playthrough to an
+     * "ONGOING" state.
+     */
     public GameWinState() {
         super(WinState.ONGOING);
     }
 
-    public void playerWin () {
+    /**
+     * Provided the appropriate conditions for the method call, set the game
+     * instance to "WIN".
+     */
+    public void playerWin() {
         this.value = WinState.PLAYER_WIN;
     }
 
-    public void playerLose ()  {
+    /**
+     * Set the state of the game to "LOSE".
+     */
+    public void playerLose()  {
         this.value = WinState.PLAYER_LOSE;
     }
-
 }
